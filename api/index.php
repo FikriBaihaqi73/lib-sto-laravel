c0e4f5b2409ecb96cd31b8d60440b1ac69a7e6c5
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2026-02-16 08:19:16 +0700

    debug: add error reporting for vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (!extension_loaded('sodium')) {
+    // echo "Extension 'sodium' is not loaded!";
+}
+
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    echo "Autoloader not found! Did composer install run?";
+    exit;
+}
+
+
 require __DIR__ . '/../public/index.php';
